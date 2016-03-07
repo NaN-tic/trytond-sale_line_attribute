9f97c56c8997eb6f33a896fec673d78dbaaaa481
jmartin <jmartin@zikzakmedia.com>
2016-03-07 12:08:23 +0100

    Add all_attributes functional field to sale line

--- sale.py
+++ sale.py
@@ -1,10 +1,18 @@
 # This file is part of the sale_line_attribute module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import PoolMeta
-
+from collections import OrderedDict
 from trytond.model import fields
 from trytond.pyson import Bool, Eval, Not
+
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
+
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
 
 
 __all__ = ['SaleLine']
@@ -19,6 +27,8 @@ class SaleLine:
             ],
         depends=['attribute_set'])
     attribute_set = fields.Many2One('product.attribute.set', 'Attribute Set')
+    all_attributes = fields.Function(fields.Text('Attributes'),
+        'get_all_attributes')
 
     @fields.depends('attribute_set', 'product')
     def on_change_product(self):
@@ -33,3 +43,32 @@ class SaleLine:
             ('//page[@id="attributes"]', 'states', {
                     'invisible': Not(Bool(Eval('attribute_set', 0))),
                     })]
+
+    @classmethod
+    def get_all_attributes(cls, lines, name):
+        ProductAttribute = Pool().get('product.attribute')
+        res = {l.id: '' for l in lines}
+        for line in lines:
+            party = line.sale and line.sale.party or line.party
+            party_lang = None
+            if party:
+                party_lang = party.lang and party.lang.code
+            if not party or not party_lang:
+                party_lang = 'en_US'
+            with Transaction().set_context({'language': party_lang}):
+                attributes = ProductAttribute.search([])
+                attributes = {p.name: p for p in attributes}
+                translated_line = cls(line.id)
+                if translated_line.attributes:
+                    for attr in translated_line.attributes:
+                        attribute = attributes.get(attr)
+                        if attribute:
+                            label = attribute.string
+                            value = line.attributes[attr]
+                            if attribute.type_ != 'boolean' and not value:
+                                continue
+                            if attribute.type_ == 'selection':
+                                value = dict(json.loads(
+                                        attribute.selection_json))[value]
+                            res[line.id] += ('%s: %s\n' % (label, value))
+        return res
